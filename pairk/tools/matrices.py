68bf1a1b023376d6bca60a9b3b39dfba4def061f
Jackson Halpin <23747834+jacksonh1@users.noreply.github.com>
2024-06-11 20:19:14 -0400

    added matrices

---
+++ pairk/tools/matrices.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+from Bio import Align
+from importlib_resources import files
+
+# MATRIX_DIR = Path(str(files('pairk.matrices')))
+# AVAILABLE_MATRIX_FILES = {i.stem: i for i in MATRIX_DIR.glob("*") if not i.name.endswith(".py") and not i.name.startswith("__")}
+AVAILABLE_MATRIX_FILES = {i.stem: i for i in files('pairk.data.matrices').iterdir()}
+
+BIOPYTHON_MATRICES = Align.substitution_matrices.load()
+AA_ORDER = ['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*']
+
+def convert_matrix_array_2_df(matrix_array: Align.substitution_matrices.Array) -> pd.DataFrame:
+    # convert to pandas dataframe
+    aas = list(set([i[0] for i in matrix_array.keys()]))
+    # sort aas to match the order in AA_ORDER
+    aas = sorted(aas, key=lambda x: AA_ORDER.index(x))
+    mat_df = pd.DataFrame(index=aas, columns=aas)
+    for k in matrix_array.keys():
+        mat_df.loc[k[0], k[1]] = matrix_array[k]
+    mat_df = mat_df.astype(float)
+    return mat_df
+
+
+def load_matrix_as_df(matrix_name: str) -> pd.DataFrame:
+    # convert to pandas dataframe
+    if matrix_name in BIOPYTHON_MATRICES:
+        mat = Align.substitution_matrices.load(matrix_name)
+        return convert_matrix_array_2_df(mat)
+    try:
+        mat = Align.substitution_matrices.read(AVAILABLE_MATRIX_FILES[matrix_name])
+        return convert_matrix_array_2_df(mat)
+    except AssertionError:
+        return pd.read_csv(AVAILABLE_MATRIX_FILES[matrix_name], index_col=0)
+
+
+def load_matrix_for_aligner(matrix_name: str) -> pd.DataFrame:
+    # convert to pandas dataframe
+    if matrix_name in BIOPYTHON_MATRICES:
+        return Align.substitution_matrices.load(matrix_name)
+    try:
+        return Align.substitution_matrices.read(AVAILABLE_MATRIX_FILES[matrix_name])
+    except AssertionError:
+        raise ValueError(f"matrix {matrix_name}: ({AVAILABLE_MATRIX_FILES[matrix_name]}) is probably not compatible with Bio.Align Aligner")
+
+
+def matrix_df_to_dict(matrix_df: pd.DataFrame) -> dict[str, dict[str, float]]:
+    '''
+    It's much faster to access a dictionary than a pandas dataframe
+    '''
+    matrix_dict = {}
+    for i, row in matrix_df.iterrows():
+        if i in matrix_dict:
+            raise ValueError(f"Duplicate key {i}")
+        matrix_dict[i] = {}
+        for j, val in row.items():
+            matrix_dict[i][j] = val
+    return matrix_dict
+
+
+def print_available_matrices():
+    print("biopython-builtin matrices (aligner compatible):")
+    for k in BIOPYTHON_MATRICES:
+        print(k)
+    print("\nother matrices:")
+    for k, v in AVAILABLE_MATRIX_FILES.items():
+        print(f'{k}\n - {v}')
